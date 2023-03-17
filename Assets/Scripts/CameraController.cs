f80bc6a09cd18130291278fbbd240ddc3f61cb7d
Jacob <jakehub24@gmail.com>
2023-03-17 11:09:47 -0400

    First steps

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("shrek")]
+    [SerializeField] private GameObject[] cameraArray;
+    [SerializeField] private int cameraCount = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
